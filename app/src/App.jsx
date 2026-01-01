c401a312ecafe1c6e971e68151f7aebea7020089
goku007007 <goku.careers@gmail.com>
2025-12-31 22:19:02 -0600

    feat: add SPA page tracking for Google Analytics

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import Header from './components/layout/Header';
 import Home from './pages/Home';
@@ -9,28 +10,50 @@ import Business from './pages/Business';
 import NotFound from './pages/NotFound';
 import './styles/global.css';
 
+// Google Analytics page tracking for SPA
+function usePageTracking() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-7G0EZG70SP', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+}
+
+function AppContent() {
+  usePageTracking();
+
+  return (
+    <>
+      <a href="#main-content" className="skip-to-content">
+        Skip to main content
+      </a>
+      <Header />
+      <main id="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/playground" element={<Playground />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="/business" element={<Business />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <a href="#main-content" className="skip-to-content">
-          Skip to main content
-        </a>
-        <Header />
-        <main id="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/calculator" element={<Calculator />} />
-            <Route path="/playground" element={<Playground />} />
-            <Route path="/docs" element={<Docs />} />
-            <Route path="/business" element={<Business />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
+        <AppContent />
       </BrowserRouter>
     </ThemeProvider>
   );
 }
 
 export default App;
-
